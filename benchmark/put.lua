17c1d0e4dadc8a004145f559412b05ebb2e94a9d
Alex <alex.podikoglou@gmail.com>
2024-07-10 21:13:13 +0300

    fix: fix bench script

--- benchmark/put.lua
+++ benchmark/put.lua
@@ -1,3 +1,13 @@
-wrk.method = "PUT"
-wrk.body   = "value=" .. tostring(math.random(1000, 1000000))
-wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+function request()
+    wrk.method = "PUT"
+    wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+    wrk.body = "value=" .. tostring(math.random(1000, 1000000))
+
+    return wrk.format()
+end
+
+-- function request()
+--     counter = counter + 1
+
+--     return wrk.request()
+-- end
